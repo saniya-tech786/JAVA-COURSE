a1f40ed6c985683b048337d71ea83e65a5f93b47
Saniya Shaikh <saniya.shaikh.web@gmail.com>
2026-01-17 22:09:39 +0530

    Add files via upload

---
+++ String_methods.java
@@ -0,0 +1,13 @@
+public class String_methods {
+    public static void main(String[] args) {
+        String name="java Programming   ";
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.trim());
+        System.out.println(name.length());
+        System.out.println(name.substring(2));
+        System.out.println(name.substring(2, 6));
+        System.out.println(name.replace('a','k'));
+    }
+    
+}
---
+++ String_object.java
@@ -0,0 +1,15 @@
+public class String_object {
+    public static void main(String[] args) {
+        char[] c={'S','A','N','A'};
+        byte[] b={65,66,67,68};
+
+          String b1=new String(b);
+           String s1=new String(c,1,2);
+           String s=new String("java programe");
+           System.out.println(s1);
+           System.out.println(b1);
+           System.out.println(s);
+        }
+    }
+    
+
